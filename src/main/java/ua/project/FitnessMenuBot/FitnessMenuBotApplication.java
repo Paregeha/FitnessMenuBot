9b28d4ba724bd6d9ef7c37b6128dfb357774e26f
OleksandrPerehinchuk <paregeha@gmail.com>
2023-08-02 14:37:20 +0300

    Init bot for transfer on windows

---
+++ src/main/java/ua/project/FitnessMenuBot/FitnessMenuBotApplication.java
@@ -0,0 +1,13 @@
+package ua.project.FitnessMenuBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessMenuBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessMenuBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ua/project/FitnessMenuBot/FitnessMenuBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.project.FitnessMenuBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessMenuBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
